62e299d3979dc60e4868741aad4c7fb0b77c86c5
nacho <nacho.mota@gmail.com>
2022-09-19 18:18:35 +0200

    working

--- sources/src/get_next_line.c
+++ sources/src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 17:30:14 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/09/19 18:01:03 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/09/19 18:17:03 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -89,19 +91,20 @@ int	ft_tp_line(t_print *tp, char **line)
 
 int	ft_buffer(int fd, t_print *tp, char **line)
 {
-	int	size_buf;
+	int	size;
 
+	size = 0;
 	if (!*tp->content)
-		size_buf = read(fd, tp->content, BUFFER_SIZE);
-	if (size_buf < 0)
+		size = read(fd, tp->content, BUFFER_SIZE);
+	if (size < 0)
 		return (-1);
 	if (*tp->content)
 		ft_tp_line(tp, (&(*line)));
-	if ((!size_buf && !*line) || (!*tp->content && !size_buf && *line))
+	if ((!size && !*line) || (!*tp->content && !size && *line))
 		return (0);
 	else if (!ft_strchr(*line, '\n'))
 		ft_buffer(fd, tp, &(*line));
-	return (size_buf);
+	return (size);
 }
 
 char	*get_next_line(int fd)
@@ -136,9 +139,9 @@ char	*get_next_line(int fd)
 // 	int		fd;
 // 	char *line;
 
-// 	// fd = open("../test/text-copy.txt", O_RDONLY);
+// 	fd = open("../test/text-copy.txt", O_RDONLY);
 // 	// fd = open("../test/nl", O_RDONLY);
-// 	fd = open("../test/multiple_nl.txt", O_RDONLY);
+// 	// fd = open("../test/multiple_nl.txt", O_RDONLY);
 // 	// close(fd);
 
 // 	// if (fd == -1)
